e54987fc6dc5e8922fbd3cb74d550d040fdcfbcb
AzureAD\RanjithaAmirthalinga <ranjitha.amirthalingam@syncfusion.com>
2023-09-15 20:55:13 +0530

    committed the changes.

--- angular/default/src/app.component.ts
+++ angular/default/src/app.component.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { LinkAnnotationService, BookmarkViewService, MagnificationService,
-         ThumbnailViewService, ToolbarService, NavigationService,
-         AnnotationService, TextSearchService, TextSelectionService,
-         PrintService
-       } from '@syncfusion/ej2-angular-pdfviewer';
+import { Component, ViewEncapsulation, OnInit,ViewChild} from '@angular/core';
+import {
+    PdfViewerComponent, LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService,
+    ToolbarService, NavigationService, TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService
+} from '@syncfusion/ej2-angular-pdfviewer';
+import { SwitchComponent } from '@syncfusion/ej2-angular-buttons';
+import { ClickEventArgs } from '@syncfusion/ej2-buttons';
 
+/**
+ * Default PdfViewer Controller
+ */
 @Component({
-  selector: 'app-root',
-  // specifies the template string for the PDF Viewer component
-  template: `<div class="content-wrapper">
-                <ejs-pdfviewer id="pdfViewer"
-                       [documentPath]='document'
-                       style="height:640px;display:block">
-                </ejs-pdfviewer>
-             </div>`,
-  providers: [ LinkAnnotationService, BookmarkViewService, MagnificationService,
-               ThumbnailViewService, ToolbarService, NavigationService,
-               AnnotationService, TextSearchService, TextSelectionService,
-               PrintService]
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    encapsulation: ViewEncapsulation.None,
+    // tslint:disable-next-line:max-line-length
+    providers: [LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService, NavigationService, 
+                TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService],
+    styleUrls: ['app.component.css'],
 })
-export class AppComponent implements OnInit {
-  public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
-  ngOnInit(): void {
-  }
+
+export class AppComponent {
+    @ViewChild('pdfviewer')
+    public pdfviewerControl: PdfViewerComponent;
+    @ViewChild('switch')
+    public switch: SwitchComponent;
+    
+    public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+    ngOnInit(): void {
+        // ngOnInit function
+    }
+    public change(e: any): void {
+        if (e.checked) {
+            this.pdfviewerControl.serviceUrl = '';
+        }
+        else {
+            this.pdfviewerControl.serviceUrl = 'https://ej2services.syncfusion.com/angular/development/api/pdfviewer';
+        }
+        this.pdfviewerControl.dataBind();
+        this.pdfviewerControl.load(this.pdfviewerControl.documentPath, null);
+    }
 }
