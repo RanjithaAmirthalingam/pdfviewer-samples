eeda5b751916ed02c8e97f9bd57734652c5c05fe
AzureAD\RanjithaAmirthalinga <ranjitha.amirthalingam@syncfusion.com>
2023-09-19 18:58:29 +0530

    added

--- react/default/src/index.js
+++ react/default/src/index.js
@@ -1,41 +1,24 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner, Inject } from '@syncfusion/ej2-react-pdfviewer';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, 
+         ThumbnailView, Print,TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject } from '@syncfusion/ej2-react-pdfviewer';
 
-import { SwitchComponent } from '@syncfusion/ej2-react-buttons';
+function App() {
+return (<div>
+   <div className='control-section'>
+      <PdfViewerComponent 
+            id="container" 
+            documentPath="PDF_Succinctly.pdf"
+            serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/pdfviewer" 
+            style={{ 'height': '640px' }}>
 
-function Default() {
-    let viewer;
-    return (<div>
-        <div className='control-section'>
-                <div className="flex-container">
-                <label htmlFor="checked" className="switchLabel"> Standalone PDF Viewer </label>
-                 <div className="e-message render-mode-info">
-                     <span className="e-msg-icon render-mode-info-icon" title="Turn OFF to render the PDF Viewer as server-backed"></span>
-                 </div>
-                    <div>
-                    <SwitchComponent cssClass="buttonSwitch" id="checked" change={change} checked={true}></SwitchComponent>
-                    </div>
-                </div>
-            {/* Render the PDF Viewer */}
-            <PdfViewerComponent ref={(scope) => { viewer = scope; }} id="container" documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf" style={{ 'height': '640px' }}>
-                <Inject services={[Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner]}/>
-            </PdfViewerComponent>
-        </div>
-    </div>);
-    function change(args) {
-        if (args.checked) {
-            viewer.serviceUrl = '';
-        }
-        else {
-            viewer.serviceUrl = 'https://ej2services.syncfusion.com/react/development/api/pdfviewer';
-        }
-        viewer.dataBind();
-        viewer.load(viewer.documentPath, null);
-    }
+               <Inject services={[ Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, 
+                                   ThumbnailView, Print, TextSelection, TextSearch, FormFields, FormDesigner ]}/>
+
+      </PdfViewerComponent>
+   </div>
+</div>);
 }
-export default Default;
-
-const root = createRoot(document.getElementById('sample'));
-root.render(<Default />);
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
